74f0cb3ac258183f4baf9cb6d0a760ab33e9d215
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-02 03:37:29 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,9 +9,7 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    if @backed_projects << project
-      Project.add_backer.new
-    end
+    project.backers << self
   end
 
 
